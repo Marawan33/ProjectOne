855c25ecb7569bb5d69bb6d423485506f48ebde9
Marawan33 <marawan.mohamed112233@gmail.com>
2024-09-08 21:02:39 +0300

    Part 02 Map-MapGet-MapPost

--- Program.cs
+++ Program.cs
@@ -6,8 +6,24 @@ namespace MVCDemo
         {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/Home", async context =>
+                {
+                    await context.Response.WriteAsync("You are At Home Page");
+                }
+                );
+                endpoints.MapGet("/products", async context =>
+                {
+                    await context.Response.WriteAsync("You Are At Products Page");
+                }
+                );
+            });
 
-            app.MapGet("/", () => "Hello World!");
+            // app.MapGet("/", () => "Hello World!");
+
+            app.Run(async(HttpContext) => { await HttpContext.Response.WriteAsync("Your Requested Page Is Not Available"); });
 
             app.Run();
         }
